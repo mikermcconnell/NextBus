4a0ee851f6f45d9dae1eeaf0b8f96a1a75bf93dc
mikermcconnell <80167613+mikermcconnell@users.noreply.github.com>
2025-07-07 13:39:18 -0400

    Fix Vercel build: remove unused ts-expect-error, add eslint-config-next devDep

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,5 +2,4 @@ import '@testing-library/jest-dom';
 
 /// <reference types="jest" />
 
-// @ts-expect-error jest global provided by test environment
 jest.mock('next/image', () => ({ __esModule: true, default: () => null })); 
